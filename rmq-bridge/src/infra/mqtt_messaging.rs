124ab5fa8c6fd5e48c8a9fe9e8cb066f4d12ecc0
PedroLouback <pcampos952@gmail.com>
2024-01-22 23:09:05 -0300

    add rmq-bridge complete

---
+++ rmq-bridge/src/infra/mqtt_messaging.rs
@@ -0,0 +1,116 @@
+use futures_util::StreamExt;
+use log::{error, info};
+use paho_mqtt::{AsyncClient, ConnectOptionsBuilder, CreateOptionsBuilder, Message};
+use std::env::var;
+
+use crate::services::{messages::MQTTMessage, service::BridgeService};
+
+struct MQTTConfigs {
+    protocol: String,
+    host: String,
+    port: String,
+    client_id: String,
+}
+
+pub struct MQTTMessaging {
+    subscribes: Vec<(String, u8)>,
+    service: Box<dyn BridgeService>,
+}
+
+impl MQTTMessaging {
+    pub fn new(service: Box<dyn BridgeService>) -> Self {
+        MQTTMessaging {
+            subscribes: vec![],
+            service,
+        }
+    }
+}
+
+impl MQTTMessaging {
+    fn envs(&self) -> Result<MQTTConfigs, ()> {
+        let Ok(host) = var("MQTT_HOST") else {
+            error!("failure to read the MQTT_HOST env");
+            return Err(());
+        };
+
+        let Ok(port) = var("MQTT_PORT") else {
+            error!("failure to read the MQTT_PORT env");
+            return Err(());
+        };
+
+        let Ok(protocol) = var("MQTT_PROTOCOL") else {
+            error!("failure to read the MQTT_PROTOCOL env");
+            return Err(());
+        };
+
+        let Ok(client_id) = var("MQTT_CLIENT_ID") else {
+            error!("failure to read the MQTT_CLIENT_ID env");
+            return Err(());
+        };
+
+        Ok(MQTTConfigs {
+            client_id,
+            host,
+            port,
+            protocol,
+        })
+    }
+
+    pub async fn connect(&self) -> Result<(), ()> {
+        let env = self.envs()?;
+
+        let configs = CreateOptionsBuilder::new()
+            .server_uri(format!("{}://{}:{}", env.protocol, env.host, env.port))
+            .client_id(env.client_id)
+            .finalize();
+
+        let Ok(mut client) = AsyncClient::new(configs) else {
+            error!("failure to create MQTT AsyncClient instance");
+            return Err(());
+        };
+
+        // Aqui será setado o username e password
+        let conn_opts = ConnectOptionsBuilder::new().finalize();
+
+        let Ok(_) = client.connect(Some(conn_opts)).await else {
+            error!("failure to connect to MQTT");
+            return Err(());
+        };
+
+        for (topic, qos) in self.subscribes.clone() {
+            client.subscribe(topic, qos.into());
+        }
+
+        let mut stream = client.get_stream(2048);
+
+        while let Some(opt_infos) = stream.next().await {
+            self.handler(opt_infos);
+        }
+
+        Ok(())
+    }
+
+    pub fn subscribe(&mut self, topic: String, qos: u8) {
+        self.subscribes.push((topic, qos))
+    }
+
+    fn handler(&self, infos: Option<Message>) {
+        let Some(message) = infos else {
+            return;
+        };
+
+        let payload = message.payload();
+
+        let Ok(msg) = serde_json::from_slice::<MQTTMessage>(payload) else {
+            error!("failed to deserialized message!");
+            return;
+        };
+
+        match self.service.exec(&msg) {
+            Ok(_) => {
+                info!("message processed succeessfully!");
+            }
+            Err(_) => error!("failed to process message"),
+        };
+    }
+}
---
+++ rmq-bridge/src/main.rs
@@ -0,0 +1,26 @@
+mod infra;
+mod services;
+
+use log::info;
+
+use crate::{infra::mqtt_messaging::MQTTMessaging, services::service::BridgeServiceImpl};
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().expect("failure to read .env");
+    env_logger::init();
+
+    info!("starting application...");
+
+    let service = BridgeServiceImpl::new();
+    let mut messaging = MQTTMessaging::new(Box::new(service));
+
+    messaging.subscribe("HedroTraining2024/#".into(), 2);
+
+    messaging
+        .connect()
+        .await
+        .expect("failure to connect to MQTT");
+
+    info!("MQTT connected!")
+}
